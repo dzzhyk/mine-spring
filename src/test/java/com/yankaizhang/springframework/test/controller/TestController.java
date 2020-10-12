2210ead81f6aa042b9ddaf4c6e078547e8b4ddc5
dzzhyk <1354839386@qq.com>
2020-10-12 11:29:58 +0800

    调整包结构，剔除lombok支持

--- src/test/java/com/yankaizhang/springframework/test/controller/TestController.java
+++ src/test/java/com/yankaizhang/springframework/test/controller/TestController.java
@@ -1,19 +1,17 @@
-package test.controller;
+package com.yankaizhang.springframework.test.controller;
 
 import com.yankaizhang.springframework.annotation.Autowired;
 import com.yankaizhang.springframework.annotation.Controller;
 import com.yankaizhang.springframework.annotation.RequestMapping;
 import com.yankaizhang.springframework.annotation.RequestParam;
-import lombok.extern.slf4j.Slf4j;
-import test.service.TestService;
+import com.yankaizhang.springframework.test.service.TestService;
 import com.yankaizhang.springframework.webmvc.ModelAndView;
-import test.service.impl.UserServiceImpl;
+import com.yankaizhang.springframework.test.service.impl.UserServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
-@Slf4j
 @Controller
 public class TestController {
 
@@ -39,5 +37,10 @@ public class TestController {
         map.put("answer", result);
         ModelAndView modelAndView = new ModelAndView("hello", map);     // 返回到hello.html
         return modelAndView;
+    }
+
+    @RequestMapping("/index")
+    public String toIndex(HttpServletRequest req, HttpServletResponse resp){
+        return "index";
     }
 }
